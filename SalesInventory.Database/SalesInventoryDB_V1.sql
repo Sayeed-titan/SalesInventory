a73a687f747681e2b5bc7d4b74a071b987d085dd
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-29 09:34:27 +0600

    Update SalesInventoryDB_V1.sql
    Add products, customers table

--- SalesInventory.Database/SalesInventoryDB_V1.sql
+++ SalesInventory.Database/SalesInventoryDB_V1.sql
@@ -1,6 +1,5 @@
 -- Version 1: Database Schema WITHOUT Indexes
 -- Database: SalesInventoryDB_V1
-
 USE master;
 GO
 
@@ -22,5 +21,33 @@ CREATE TABLE Categories(
 	CategoryId INT PRIMARY KEY IDENTITY(1,1),
 	CategoryName NVARCHAR(100) NOT NULL,
 	Description NVARCHAR(500),
+	CreatedDate DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+-- 2. Products Table
+CREATE TABLE Products(
+	ProductId INT PRIMARY KEY IDENTITY(1,1),
+	ProductName NVARCHAR(200) NOT NULL, 
+	-- Column Level Constraints : REFERENCES Categories(CategoryId)
+	CategoryId INT NOT NULL,
+	UnitPrice DECIMAL(18,2) NOT NULL,
+	StockQuantity INT NOT NULL DEFAULT 0,
+	ReorderLevel INT NOT NULL DEFAULT 10,
+	IsActive BIT NOT NULL DEFAULT 1,
+	CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+	-- Add Foreign Key for CategoryId coming from Category Table
+	-- Table Level Constraints
+	FOREIGN KEY (CategoryId) REFERENCES Categories(CategoryId)
+);
+
+-- 3. Customers Table
+CREATE TABLE Customers (
+	CustomerId INT PRIMARY KEY IDENTITY (1,1),
+	CustomerName NVARCHAR(200) NOT NULL, 
+	Email NVARCHAR(100), 
+	Phone NVARCHAR(20),
+	Address NVARCHAR(500),
+	City NVARCHAR(100),
+	Country NVARCHAR(100),
 	CreatedDate DATETIME NOT NULL DEFAULT GETDATE()
 );
