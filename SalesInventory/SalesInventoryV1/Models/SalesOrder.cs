7bae3e39fff3239c3c0127f23f626e76aafc03b3
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-31 22:12:07 +0600

    Add salesorder model class

---
+++ SalesInventory/SalesInventoryV1/Models/SalesOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System . Collections . Generic;
+using System . ComponentModel . DataAnnotations;
+using System . ComponentModel . DataAnnotations . Schema;
+using System . Linq;
+using System . Web;
+
+namespace SalesInventoryV1 . Models
+{
+      [Table ( "SalesOrders" )]
+      public class SalesOrder
+      {
+            [Key]
+            public int OrderId { get; set; }
+
+            [Required]
+            [StringLength ( 50 )]
+            public string OrderNumber { get; set; }
+
+            public int CustomerId { get; set; }
+            public DateTime Orderdate { get; set; }
+
+            [Column ( TypeName = "decimal(18, 2)" )]
+            public decimal TotalAmount { get; set; }
+
+            [StringLength ( 50 )]
+            public string Status { get; set; }
+
+            public DateTime CreatedDate { get; set; }
+
+            public virtual Customer Customer { get; set; }
+
+      }
+}
