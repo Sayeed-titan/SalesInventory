95649dfe618db13bfb53125fadf8c400bfff78b7
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-31 13:26:21 +0600

    Update SalesInventoryDB_V1.sql
    Added Tales SalesOrder

--- SalesInventory.Database/SalesInventoryDB_V1.sql
+++ SalesInventory.Database/SalesInventoryDB_V1.sql
@@ -51,3 +51,26 @@ CREATE TABLE Customers (
 	Country NVARCHAR(100),
 	CreatedDate DATETIME NOT NULL DEFAULT GETDATE()
 );
+
+-- 4. Sales Orders Table
+CREATE TABLE SalesOrders (
+	OrderId INT PRIMARY KEY IDENTITY(1,1),
+	OrderNumber NVARCHAR(50) NOT NULL UNIQUE,
+	CustomerId INT NOT NULL, 
+	OrderDate DATETIME NOT NULL DEFAULT GETDATE(), 
+	TotalAmount DECIMAL(18,2) NOT NULL,
+	Status NVARCHAR(50) NOT NULL DEFAULT 'Pending', -- Status can be Pending, Completed, Cancelled
+	CreatedDate DATETIME NOT NULL DEFAULT GETDATE()
+	FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)	
+);
+
+-- This shows all FK relationships.
+-- You can then see which tables have no references
+SELECT 
+    fk.name AS ForeignKeyName,
+    tp.name AS ParentTable,
+    tr.name AS ReferencedTable
+FROM sys.foreign_keys AS fk
+INNER JOIN sys.tables AS tp ON fk.parent_object_id = tp.object_id
+INNER JOIN sys.tables AS tr ON fk.referenced_object_id = tr.object_id
+ORDER BY tr.name;
