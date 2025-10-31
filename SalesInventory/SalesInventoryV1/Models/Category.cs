935d9a4e30d02201ee8aaafcbf87a8ad5271018b
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-31 22:04:33 +0600

    Add product  mode class Add category model class

---
+++ SalesInventory/SalesInventoryV1/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System . Collections . Generic;
+using System . ComponentModel . DataAnnotations;
+using System . ComponentModel . DataAnnotations . Schema;
+using System . Linq;
+using System . Web;
+
+namespace SalesInventoryV1 . Models
+{
+      [Table("Categories")]
+      public class Category
+      {
+            [Key]
+            public int CategoryId { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string CategoryName { get; set; }
+
+            [StringLength(500)]
+            public string Description { get; set; }
+
+            public DateTime CreatedDate { get; set; }
+
+            public virtual ICollection<Product> Products { get; set; }
+      }
+}
